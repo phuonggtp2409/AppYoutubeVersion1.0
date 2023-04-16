10e9ef11512ec65552c345c57840492ae1a52123
huyitviet <huyitviet@gmail.com>
2023-04-16 17:53:08 +0700

    Initial Commit

---
+++ AppYoutube/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppYoutube
+//
+//  Created by VanHuy on 2023-04-16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
